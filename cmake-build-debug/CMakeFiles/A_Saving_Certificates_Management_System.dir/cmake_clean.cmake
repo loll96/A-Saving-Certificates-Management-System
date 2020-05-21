3c90895583b02cd88fac847e9e35c2d1787f3683
Ali Mamdouh <45511037+loll96@users.noreply.github.com>
2020-05-21 02:38:37 +0200

    start

---
+++ cmake-build-debug/CMakeFiles/A_Saving_Certificates_Management_System.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "A_Saving_Certificates_Management_System.exe"
+  "A_Saving_Certificates_Management_System.exe.manifest"
+  "A_Saving_Certificates_Management_System.pdb"
+  "CMakeFiles/A_Saving_Certificates_Management_System.dir/main.cpp.obj"
+  "libA_Saving_Certificates_Management_System.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/A_Saving_Certificates_Management_System.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
